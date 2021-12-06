f1c417a477c8e69eaceaa5074c98c4ef7b2fd21f
agmeadows <65190360+agmeadows@users.noreply.github.com>
2021-12-06 01:10:45 -0500

    changes

--- static/js/app.js
+++ static/js/app.js
@@ -28,38 +28,47 @@ var filters = [];
 
 // 3. Use this function to update the filters. 
 function updateFilters(fieldData) {
-    let filteredData = tableData
-    
-    // 4a. Save the element that was changed as a variable.
-    fieldElem = d3.select(fieldData.element)
+    console.log("in func")
+  
     // 4b. Save the value that was changed as a variable.
     fieldVal = fieldData.value;
     // 4c. Save the id of the filter that was changed as a variable.
     fieldID = fieldData.id
-  
     console.log(fieldID)
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
+
     
-    filteredData = filteredData.filter(row => row.fieldID === fieldVal);
-  
+    console.log(filteredData)
     // 6. Call function to apply all filters and rebuild the table
-    filterTable();
+    buildTable(filteredData);
   
   }
   
   // 7. Use this function to filter the table when data is entered.
-  function filterTable() {
-  
+  function filterTable(fieldData) {
+
+    console.log(fieldData)
     // 8. Set the filtered data to the tableData.
-    
+    let filteredData = tableData
   
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    
+    filteredData = filteredData
+      .filter(row => row.datetime === fieldData[0]["value"] &&
+      row.city === fieldData[1]["value"]
+        
+      );
+/*
+      row.city === fieldData[1]["value"] && 
+      row.state === fieldData[2]["value"] && 
+      row.country === fieldData[3]["value"] && 
+      row.shape === fieldData[4]["value"]
+      */
+    console.log(filteredData)
   
     // 10. Finally, rebuild the table using the filtered data
-    
+    buildTable(filteredData);
   }
   
 // 2. Attach an event to listen for changes to each filter
@@ -78,13 +87,16 @@ console.log(fieldData)
 //fieldData.push
 d3.selectAll("input").on("change", function(){
   
-  fields = d3.selectAll("input")
-  //console.log(fields.nodes()[1])
+  fields = d3.selectAll("input");
   for (let i = 0; i <= 4; i++) {
+    console.log(fieldData[i].value)
+    console.log(fields.nodes()[i].value)
     if (fieldData[i].value != fields.nodes()[i].value)
     {
-      console.log("Field Changed")
-      updateFilters(fields.nodes()[i])
+      console.log(fields.nodes()[i].id + " Field Changed");
+      fieldData[i].value = fields.nodes()[i].value
+      filterTable(fieldData);
+      break;
     }
   } 
 });
